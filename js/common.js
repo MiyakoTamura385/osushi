2f4d0e31c9408585f3096aaa146d22e4faced51b
MiyakoT38 <tamagototuna38@gmail.com>
2021-03-07 16:43:18 +0900

    index.htmlページを更新しました

--- js/common.js
+++ js/common.js
@@ -1,20 +1,5 @@
-$('.slider').slick({
-  autoplay: true,
-  arrows: false,
-  dots: false,
-  autoplaySpeed: 1500,
-  speed: 1500,  
-});
-
-///menu html///
-//ドロワー　//
-$(document).ready(function(){
-  $('#open_nav').on('click' , function(){
-   $('#wrapper, #nav').toggleClass('show');
-  }); 
-});
-
-//試し　スライドショー////
+///// index.html ////////
+////スライドショー////
 $(function () {
   $('.slideshow').each(function () {
     var $slides = $(this).find('img'),
@@ -30,3 +15,12 @@ $(function () {
     }
   })
 })
+
+///menu html///
+//ドロワー　//
+$(document).ready(function(){
+  $('#open_nav').on('click' , function(){
+   $('#wrapper, #nav').toggleClass('show');
+  }); 
+});
+
